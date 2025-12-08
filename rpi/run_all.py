8aab2830a3516145b5bfd46a539b7e9a3ccae500
Ethan Ermovick <keg9ve@virginia.edu>
2025-12-07 19:45:35 -0500

    cleanup runall

--- rpi/run_all.py
+++ rpi/run_all.py
@@ -21,20 +21,6 @@ def redis_fio(redis_rw, fio_rw):
     redis_runner.cleanup()
     fio_runner.cleanup()
 
-# def redis_w_fio_w():
-#     os.mkdir('redis_w_fio_w')
-#     redis_runner = RedisRunner('./redis_w_fio_w/redis', [0,1], 0, {}, 10000000)
-#     redis_runner.set_writefrac(100)
-#     fio_runner = FIORunner('./redis_w_fio_w/fio', [2, 3], 2, 4096, 64, 100, '/dev/nvme0n1')
-#     redis_runner.set_writefrac(100)
-
-#     fio_runner.run(60)  # Run for 60 seconds
-#     redis_runner.run(60)  # Run for 60 seconds (duration is ignored)
-
-#     redis_runner.wait()
-#     fio_runner.wait()
-#     redis_runner.cleanup()
-#     fio_runner.cleanup()
 
 if __name__ == "__main__":
     redis_fio('read', 'read')
